b3f877b5ccb2f6fd7fa5d178c4a36cded7c7d06a
kristieelim <kristie_lim@hotmail.com>
2023-03-09 19:51:09 +0100

    Added driver page

--- src/components/DriverPage.js
+++ src/components/DriverPage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState, Fragment} from 'react'
+import data from "./appointments-mock-data.json";
 
 export default function DriverPage() {
+
+    const [appointments, setAppointments] = useState(data);
+
     return (
-        <div>
+        <div className="app-container">
             <h1>Driver Page</h1>
+            <h6>Driver Name: {appointments[0].driverName}</h6>
+            <h6>Collection Date: {appointments[0].collectionDate}</h6>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Store</th>
+                        <th>Food Type</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appointments.map((appointment) => (
+                        <Fragment>
+                            <tr>
+                                <td rowSpan={appointment.food.length + 1}>{appointment.storeName}</td>
+                            </tr>
+                            {appointment.food.map((f) => (
+                                <tr>
+                                    <td>{f.type}</td>
+                                    <td>{f.amount}</td>
+                                </tr>
+                            ))}
+                        </Fragment>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
